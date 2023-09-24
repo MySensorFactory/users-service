ca2957fa47ee6641e9cb7c188337889aaa7e821f
Damian Wójcik <damian.a.wojcik@comarch.pl>
2023-09-24 12:06:38 +0200

    Moved sql files to entity store

--- sql/v2_create_roles_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS factory_users.roles
-(
-    id uuid NOT NULL,
-    name character varying(255) NOT NULL,
-    CONSTRAINT roles_pkey PRIMARY KEY (id),
-    CONSTRAINT roles_name_key UNIQUE (name)
-);
--- src/main/java/com/factory/config/ModelMapperConfig.java
+++ src/main/java/com/factory/config/ModelMapperConfig.java
@@ -1,13 +1,38 @@
 package com.factory.config;
 
+import com.factory.openapi.model.CreateUserRequest;
+import com.factory.persistence.users.entity.User;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 @Configuration
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var mapper = new ModelMapper();
+        mapper.addConverter(createUserRequestUserConverter());
+        return mapper;
+    }
+
+    private static Converter<CreateUserRequest, User> createUserRequestUserConverter() {
+        return new AbstractConverter<>() {
+            @Override
+            protected User convert(CreateUserRequest source) {
+                if (Objects.isNull(source)) {
+                    return null;
+                }
+                var result = new User();
+                result.setEnabled(source.getEnabled());
+                result.setPassword(source.getPassword());
+                result.setEmail(source.getEmail());
+                result.setUsername(source.getUsername());
+                return result;
+            }
+        };
     }
 }
